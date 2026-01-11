d5c88e582cd062dc7f0183ef3134148fdd8e1af0
raushan24scse1430106-source <raushan.24scse1430106@gmail.com>
2026-01-11 13:04:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ if st.button("Predict Result"):
  input_data = np.array([[study_hours, attendance]])
  prediction = model.predict(input_data)
  if prediction[0] == 1:
- st.success("Prediction: PASS")
+  st.success("Prediction: PASS")
  else:
- st.error("Prediction: FAIL")
+  st.error("Prediction: FAIL")
+
